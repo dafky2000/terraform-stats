944eda26b3e8662c1ad72888afb7c5a58e5e69aa
Dan Kelly <myself@danielkelly.me>
2017-12-18 12:07:49 -0500

    Expanded api call for the entire dataset

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ let LAST_UPDATE = new Date();
 let DATA = {};
 let RENDERED_DATA = {};
 function data_update() {
-	// fetch('https://api.auction.decentraland.org/api/parcelState/range/-150,-150/150,150')
-	fetch('https://api.auction.decentraland.org/api/parcelState/range/-25,-25/25,25')
+	fetch('https://api.auction.decentraland.org/api/parcelState/range/-150,-150/150,150')
+	// fetch('https://api.auction.decentraland.org/api/parcelState/range/-25,-25/25,25')
 		.then(function(res) {
 			return res.text();
 		}).then(function(ret) {
